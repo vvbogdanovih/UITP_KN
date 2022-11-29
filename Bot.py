1ce0c2226a6942965ad9539775a34cad8bdba172
Khrystik <khrystiksoletsgo@gmail.com>
2022-11-29 19:24:06 +0200

    My commit 3

--- Bot.py
+++ Bot.py
@@ -43,31 +43,59 @@ def start(message):
 		bot.send_message(1004329909, str2)
 	else:
 		print("/send\t" + message.from_user.first_name + ", скористався адмінською командою. ID " + str(message.from_user.id))
-
+#-----------------------------РОЗКЛАД--------------------------------
 #розклад для 1 підгрупи
 @bot.message_handler(commands=['р11', 'p11'])
 def start(message):
     print("/p11\t" + message.from_user.first_name + " " + datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y"))
     bot.reply_to(message, "Підгрупа №1\nПонеділок\n\n1 пара:    -\n2 пара:  - / Сист. аналіз - Лекція\n3 пара:  Веб - Лекція\n4 пара:   -\n5 пара:  -")
 
-
 @bot.message_handler(commands=['р12', 'p12'])
 def start(message):
     print("/p12\t" + message.from_user.first_name + " " + datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y"))
     bot.reply_to(message, "Підгрупа №1\nВівторок\n\n1 пара:    - / Програмування моб. систем - Лекція\n2 пара:  ООАП - Лекція / ШНМ - Лекція\n3 пара:   Мод. систем - Лекція\n4 пара:   -\n5 пара:  -")
-
 
 @bot.message_handler(commands=['р13', 'p13'])
 def start(message):
     print("/p13\t" + message.from_user.first_name + " " + datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y"))
     bot.reply_to(message, "Підгрупа №1\nСереда\n\n1 пара:   ТПСПП\n2 пара:  Сист. аналіз\n3 пара:   ТПСПП - Лекція / Управління ІТ-проектами - Лекція\n4 пара:  Управління ІТ-проектами\n5 пара:    -")
 
-
 @bot.message_handler(commands=['р14', 'p14'])
 def start(message):
     print("/p14\t" + message.from_user.first_name + " " + datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y"))
     bot.reply_to(message, "Підгрупа №1\nЧетвер\n\n1 пара:   Веб\n2 пара:    Моделювання систем\n3 пара: ШНМ\n4 пара:    -\n5 пара:  -")
 
+@bot.message_handler(commands=['р15', 'p15'])
+def start(message):
+    print("/p15\t" + message.from_user.first_name + " " + datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y"))
+    bot.reply_to(message, "Підгрупа №1\nП'ятниця\n\n1 пара: -\n2 пара:  ООАП\n3 пара:   Програмування моб.с.\n4 пара:   -\n5 пара:  -")
+
+#розклад для 2 підгрупи
+@bot.message_handler(commands=['р21', 'p21'])
+def start(message):
+   print("/p21\t" + message.from_user.first_name + " " +  datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y"))
+   bot.reply_to(message, "Підгрупа №2\nПонеділок\n\n1 пара:    -\n2 пара:  - / Сист. аналіз - Лекція\n3 пара:  Веб - Лекція\n4 пара:   -\n5 пара:  -")
+
+@bot.message_handler(commands=['р22', 'p22'])
+def start(message):
+   print("/p22\t" + message.from_user.first_name + " " +  datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y"))
+   bot.reply_to(message, "Підгрупа №2\nВівторок\n\n1 пара:    - / Програмування моб. систем - Лекція\n2 пара:  ООАП - Лекція / ШНМ - Лекція\n3 пара:   Мод. систем - Лекція\n4 пара:   -\n5 пара:  -")
+
+@bot.message_handler(commands=['р23', 'p23'])
+def start(message):
+   print("/p23\t" + message.from_user.first_name + " " +  datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y"))
+   bot.reply_to(message, "Підгрупа №2\nСереда\n\n1 пара:    -\n2 пара:  Веб\n3 пара:    ТПСПП - Лекція / Управління ІТ-проектами - Лекція\n4 пара:  Системний аналіз\n5 пара:   ООАП")
+
+@bot.message_handler(commands=['р24', 'p24'])
+def start(message):
+   print("/p24\t" + message.from_user.first_name + " " +  datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y"))
+   bot.reply_to(message, "Підгрупа №2\nЧетвер\n\n1 пара:    -\n2 пара:  ШНМ\n3 пара:    Моделювання систем\n4 пара: ТПСПП\n5 пара:  -")
+
+@bot.message_handler(commands=['р25' , 'p25'])
+def start(message):
+   print("/p25\t" + message.from_user.first_name + " " +  datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y"))
+   bot.reply_to(message, "Підгрупа №2\nП'ятниця\n\n1 пара:    -\n2 пара:    Програмування моб. систем\n3 пара:  Управління ІТ-проектами\n4 пара:    -\n5 пара:  -")
+#-----------------------------РОЗКЛАД--------------------------------
 
 #commit in Vlad_2_Branch
 #time to start paras
@@ -147,38 +175,6 @@ def start(message):
 		bot.reply_to(message, datetime.datetime.fromtimestamp(g).strftime("%H:%M:%S") )
 
 
-
-
-@bot.message_handler(commands=['р15', 'p15'])
-def start(message):
-    print("/p15\t" + message.from_user.first_name + " " + datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y"))
-    bot.reply_to(message, "Підгрупа №1\nП'ятниця\n\n1 пара: -\n2 пара:  ООАП\n3 пара:   Програмування моб.с.\n4 пара:   -\n5 пара:  -")
-
-#розклад для 2 підгрупи
-@bot.message_handler(commands=['р21', 'p21'])
-def start(message):
-   print("/p21\t" + message.from_user.first_name + " " +  datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y"))
-   bot.reply_to(message, "Підгрупа №2\nПонеділок\n\n1 пара:    -\n2 пара:  - / Сист. аналіз - Лекція\n3 пара:  Веб - Лекція\n4 пара:   -\n5 пара:  -")
-
-@bot.message_handler(commands=['р22', 'p22'])
-def start(message):
-   print("/p22\t" + message.from_user.first_name + " " +  datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y"))
-   bot.reply_to(message, "Підгрупа №2\nВівторок\n\n1 пара:    - / Програмування моб. систем - Лекція\n2 пара:  ООАП - Лекція / ШНМ - Лекція\n3 пара:   Мод. систем - Лекція\n4 пара:   -\n5 пара:  -")
-
-@bot.message_handler(commands=['р23', 'p23'])
-def start(message):
-   print("/p23\t" + message.from_user.first_name + " " +  datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y"))
-   bot.reply_to(message, "Підгрупа №2\nСереда\n\n1 пара:    -\n2 пара:  Веб\n3 пара:    ТПСПП - Лекція / Управління ІТ-проектами - Лекція\n4 пара:  Системний аналіз\n5 пара:   ООАП")
-
-@bot.message_handler(commands=['р24', 'p24'])
-def start(message):
-   print("/p24\t" + message.from_user.first_name + " " +  datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y"))
-   bot.reply_to(message, "Підгрупа №2\nЧетвер\n\n1 пара:    -\n2 пара:  ШНМ\n3 пара:    Моделювання систем\n4 пара: ТПСПП\n5 пара:  -")
-
-@bot.message_handler(commands=['р25' , 'p25'])
-def start(message):
-   print("/p25\t" + message.from_user.first_name + " " +  datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y"))
-   bot.reply_to(message, "Підгрупа №2\nП'ятниця\n\n1 пара:    -\n2 пара:    Програмування моб. систем\n3 пара:  Управління ІТ-проектами\n4 пара:    -\n5 пара:  -")
 
 
 
