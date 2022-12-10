6437e7cd9e2c5a6356364b92b7961b17d3c00fe9
VirAgee <mizida212@gmail.com>
2022-12-11 00:27:32 +0200

    test_1_commit

--- Bot.py
+++ Bot.py
@@ -6,7 +6,7 @@ TOKEN = '1835690901:AAEwesdwqsht2mQhmn2Znp4ipSF7F9tN5no'
 bot = telebot.TeleBot(TOKEN)
 tconv = lambda x: time.strftime("%H:%M:%S %d.%m.%Y", time.localtime(x)) #Конвертацsя датb в читабельний вид
 
-#LAB8
+#LAB8_zavd
 #help comand
 @bot.message_handler(commands=['help'])
 def start(message):
